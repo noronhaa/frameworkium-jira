c70501b3118f29a931ef50c2f865216ddc21bced
noronhaa <ANoronha@Imp.net>
2018-05-29 15:41:50 +0100

    added method for create test in Zephyr

--- src/test/java/glue/GherkinParserSteps.java
+++ src/test/java/glue/GherkinParserSteps.java
@@ -6,12 +6,11 @@ import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
-import gherkin.pickles.Pickle;
 import org.testng.Assert;
 
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.List;
 import java.util.Optional;
 
 public class GherkinParserSteps {
@@ -25,9 +24,26 @@ public class GherkinParserSteps {
         System.out.println(s.isPresent());
     }
 
+    /**
+     * Make copy of a base feature which will be used as the feature under test keeping original intact
+     * @param baseFile
+     */
+    private void setupTestFeature(String baseFile){
+        String featureUnderTest = FEATURE_DIR + "featureUnderTest.feature";
+        try {
+            Files.write(Paths.get(featureUnderTest),FileUtils.readFile(baseFile).getBytes());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        featurePath = featureUnderTest;
+
+    }
+
     @Given("^I have a scenario that does NOT contain a zephyr tag$")
     public void iHaveAScenarioThatDoesNOTContainAZephyrTag() throws Throwable {
-        featurePath = "src/test/resources/gherkinParser/features/editFeature.feature";
+        String testFeature = "src/test/resources/gherkinParser/features/editFeature.feature";
+        setupTestFeature(testFeature);
     }
 
     @Given("^I have a multiple scenarios that do NOT contain a zephyr tag$")
@@ -57,7 +73,7 @@ public class GherkinParserSteps {
 
         String expectedFeature = FileUtils.readFile(expectedPath);
         String actualFeature =  FileUtils.readFile(featurePath);
-
+    //todo need to match regex? of get zID and parse that in to assert against?
         Assert.assertEquals(actualFeature,expectedFeature);
     }
 
--- src/test/resources/features/bddParser.feature
+++ src/test/resources/features/bddParser.feature
@@ -1,14 +1,14 @@
 
 Feature: checking if a bdd had a zephyr tag and adding one if needed after creating a new test in zephyr
 
-#  @gherkin
+  @gherkin
   Scenario: update single scenario with tags tha are not in zephyr
     Given I have a scenario that does NOT contain a zephyr tag
     When I parse the feature file
     Then a new zephyr test will be created
     And the scenario will be successfully updated and match "gherkinParser/features/editFeatureUpdatedExpected.feature"
 
-  @gherkin
+#  @gherkin
   Scenario: update multiple scenario with tags tha are not in zephyr
     Given I have a multiple scenarios that do NOT contain a zephyr tag
     When I parse the feature file
