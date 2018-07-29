8f094699d159bd9dc59b8503e8d5e742c256aab5
Graham Russell <graham@ham1.co.uk>
2018-07-29 19:23:20 +0100

    Simplified MethodInterceptor.

--- src/main/java/com/frameworkium/jira/listeners/MethodInterceptor.java
+++ src/main/java/com/frameworkium/jira/listeners/MethodInterceptor.java
@@ -4,11 +4,8 @@ import com.frameworkium.jira.api.SearchIssues;
 import com.frameworkium.reporting.allure.TestIdUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.testng.IMethodInstance;
-import org.testng.IMethodInterceptor;
-import org.testng.ITestContext;
+import org.testng.*;
 
-import java.lang.reflect.Method;
 import java.util.List;
 
 import static com.frameworkium.base.properties.Property.JIRA_URL;
@@ -28,57 +25,40 @@ public class MethodInterceptor implements IMethodInterceptor {
     }
 
     private List<IMethodInstance> filterTestsToRunByJQL(
-            List<IMethodInstance> methodsToBeFiltered) {
+            List<IMethodInstance> methodsToFilter) {
 
-        if (JQL_QUERY.isSpecified() && JIRA_URL.isSpecified()) {
-            logger.info("Filtering specified tests to run with JQL query results");
-
-            List<IMethodInstance> methodsWithTestIDs = methodsToBeFiltered.stream()
-                    .filter(m -> TestIdUtils.getIssueOrTmsLinkValue(m).isPresent())
-                    .collect(toList());
-
-            List<String> testIDsFromJQL =
-                    new SearchIssues(JQL_QUERY.getValue()).getKeys();
-
-            List<IMethodInstance> methodsToRun = methodsWithTestIDs.stream()
-                    .filter(m -> testIDsFromJQL.contains(
-                            TestIdUtils.getIssueOrTmsLinkValue(m).orElseThrow(IllegalStateException::new)))
-                    .collect(toList());
-
-            logTestMethodInformation(
-                    methodsToBeFiltered, methodsWithTestIDs, methodsToRun);
-
-            return methodsToRun;
-        } else {
-            // Can't run the JQL without both JIRA_URL and JQL_QUERY
-            return methodsToBeFiltered;
+        // Can't run JQL without JIRA_URL or JQL_QUERY
+        if (!JQL_QUERY.isSpecified() || !JIRA_URL.isSpecified()) {
+            return methodsToFilter;
         }
+
+        logger.info("Filtering specified tests to run with JQL query results");
+
+        List<String> testIDsFromJQL =
+                new SearchIssues(JQL_QUERY.getValue()).getKeys();
+
+        List<IMethodInstance> methodsToRun = methodsToFilter.stream()
+                .filter(m -> testIDsFromJQL.contains(
+                        TestIdUtils.getIssueOrTmsLinkValue(m).orElse("")))
+                .collect(toList());
+
+        logTestMethodInformation(methodsToRun);
+
+        return methodsToRun;
     }
 
     private void logTestMethodInformation(
-            List<IMethodInstance> methodsPreFiltering,
-            List<IMethodInstance> methodsWithTestIDs,
             List<IMethodInstance> methodsPostFiltering) {
 
         logger.debug("Running the following test methods:\n{}", () ->
                 methodsPostFiltering.stream()
-                        .map(m -> getMethodFromIMethod(m).getName())
+                        .map(this::getMethodNameFromIMethod)
                         .collect(joining("\n")));
-
-        List<String> methodsWithoutTestIds = methodsPreFiltering.stream()
-                .filter(m -> !methodsWithTestIDs.contains(m))
-                .map(m -> getMethodFromIMethod(m).getName())
-                .collect(toList());
-
-        if (methodsWithoutTestIds.size() > 0) {
-            logger.warn("The following tests don't have TestIDs {}",
-                    () -> methodsWithoutTestIds.stream().collect(joining(", ")));
-        }
 
         logger.info("Running {} tests specified by JQL query", methodsPostFiltering.size());
     }
 
-    private Method getMethodFromIMethod(IMethodInstance iMethod) {
-        return iMethod.getMethod().getConstructorOrMethod().getMethod();
+    private String getMethodNameFromIMethod(IMethodInstance iMethod) {
+        return iMethod.getMethod().getConstructorOrMethod().getMethod().getName();
     }
 }
